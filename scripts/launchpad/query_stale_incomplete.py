0e5d4057ec0d446bf63043b0a549517304994e5e
Markus Zoeller <mzoeller@de.ibm.com>
2016-04-18 15:57:58 +0200

    reuse the bugreport class for the incomplete query

--- scripts/launchpad/query_stale_incomplete.py
+++ scripts/launchpad/query_stale_incomplete.py
@@ -6,28 +6,46 @@
 # Copyright 2016 Markus Zoeller
 
 import datetime
+import logging
 import os
 
 import common
 
 PROJECT_NAME = "nova"
 DAYS_SINCE_INCOMPLETE = 30
+LOG = logging.getLogger(__name__)
 
-client = common.get_project_client(PROJECT_NAME)
-bug_tasks = client.searchTasks(status=["Incomplete"],
-                               omit_duplicates=True)
 
-print("potentially stale bugs:")
-print("=======================")
-today = datetime.datetime.today()
-counter = 0
+def get_incomplete_reports():
+    LOG.info("getting incomplete reports...")
+    client = common.get_project_client(PROJECT_NAME)
+    bug_tasks = client.searchTasks(status=["Incomplete"],
+                                   omit_duplicates=True)
+    
+    today = datetime.datetime.today()
+    reports = []
+    
+    for bug_task in bug_tasks:
+        # remove the timezone info as it disturbs the calculation of the diff
+        diff = today - bug_task.date_incomplete.replace(tzinfo=None)
+        if diff.days > DAYS_SINCE_INCOMPLETE:
+            reports.append(common.BugReport(link=bug_task.web_link,
+                                     title=bug_task.bug.title,
+                                     age=diff.days))
+    LOG.info("got incomplete reports.")
+    return reports
 
-for bug_task in sorted(bug_tasks, key=lambda bug_task: bug_task.date_incomplete):
-    # remove the timezone info as it disturbs the calculation of the diff
-    diff = today - bug_task.date_incomplete.replace(tzinfo=None)
-    if diff.days > DAYS_SINCE_INCOMPLETE:
-        print(bug_task.web_link + "  (" + str(diff.days) + " days since incomplete)")
-        counter += 1
 
-print("---------------------------------")
-print("%s potentially stale bug reports" % counter)
+def main():
+    print("potentially stale incomplete bug reports:")
+    print("=========================================")
+    reports = get_incomplete_reports()
+    for r in sorted(reports, reverse=True):
+        print(r)
+    
+    print("---------------------------------")
+    print("%s bug reports" % len(reports))
+
+
+if __name__ == '__main__':
+    main()
