387aeb22995e8250899132483e84105e058a2558
Markus Zoeller <mzoeller@de.ibm.com>
2017-03-28 18:42:39 +0200

    openstack-ansible: install more common packages

--- scripts/vagrant/openstack-ansible/centos7/provisioner/common.sh
+++ scripts/vagrant/openstack-ansible/centos7/provisioner/common.sh
@@ -4,11 +4,18 @@ logger "provisioning file is executed"
 
 echo "IP addresses: `hostname -I`"
 
+# yum upgrade
+
+yum install -y git
+yum install -y openssh-server
+yum install -y python-devel
+
 yum install -y epel-release
 
 yum install -y redhat-lsb
 
 yum install -y ntp
+yum install -y ntpdate
 systemctl enable ntpd
 systemctl start ntpd
 
