3b22f1f912704c76ffb4b70eca1bfc7f6cadb4c9
Markus Zoeller <mzoeller@de.ibm.com>
2017-03-31 16:08:53 +0200

    vagrant: ansible-openstack: add more memory + swapfile

--- scripts/vagrant/openstack-ansible/centos7/test-aio/provisioning/shell-provisioner.sh
+++ scripts/vagrant/openstack-ansible/centos7/test-aio/provisioning/shell-provisioner.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# ====================
+# Extend the root disk
+# ====================
 if [ ! -d "/dev/sdb1" ]; then
     echo "increasing disk size..."
     #    fdisk /dev/sdb
@@ -18,4 +21,20 @@ if [ ! -d "/dev/sdb1" ]; then
     lvextend -L +60G /dev/VolGroup00/LogVol00  # extend logical volume
     xfs_growfs /dev/VolGroup00/LogVol00   # grow the xfs filesystem in the logical volume
     df -h /   # has now more space
+fi
+
+
+# ===============
+# Add a swap file
+# ===============
+SWAPFILE_SIZE_MB=8192
+has_swap=`grep "swapfile" /etc/fstab`
+if [ -z "$has_swap" ]; then
+    # fallocate -l ${SWAPFILE_SIZE_MB}M /swapfile
+    # http://unix.stackexchange.com/questions/294600/i-cant-enable-swap-space-on-centos-7
+    sudo dd if=/dev/zero of=/swapfile count={SWAPFILE_SIZE_MB} bs=1MiB
+    chmod 600 /swapfile
+    mkswap /swapfile
+    swapon /swapfile
+    echo '/swapfile none swap defaults 0 0' >> /etc/fstab
 fi
